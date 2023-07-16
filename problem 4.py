6114a57a19beef3e7881626cd5a358871bf1a174
Shaik Thaheer Hussain <117450405+thaheerhussain18@users.noreply.github.com>
2023-07-17 00:30:57 +0530

    Update problem 4.py

--- problem 4.py
+++ problem 4.py
@@ -1,4 +1,5 @@
-val=[1,2,8,9,12,46,76,82,15,20,30]
+# testexample=[1,2,8,9,12,46,76,82,15,20,30]
+val=list(map(int,input().split()))
 dic={}
 for i in range(1,10):
     c=0
@@ -7,3 +8,4 @@ for i in range(1,10):
             c+=1
     dic[i]=c
 print(dic)
+
